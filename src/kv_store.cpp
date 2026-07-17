118a35e57c076682528270c85867fd3d5b715731
Alexander Vinarsky <vinarskydichtv@gmail.com>
2026-07-17 23:59:26 +0300

    btree testing update

--- src/kv_store.cpp
+++ src/kv_store.cpp
@@ -52,3 +52,100 @@ void test_kv_store_overwrite() {
     }
     std::remove(kTestDbPath);
 }
+
+void test_kv_store_size_empty() {
+    std::remove(kTestDbPath);
+    {
+        KvStore db(kTestDbPath);
+
+        CHECK(db.empty());
+        CHECK(db.size() == 0);
+
+        db.put("test1", "symbol:1");
+        CHECK(!db.empty());
+        CHECK(db.size() == 1);
+
+        db.put("test67", "symbol:67");
+        CHECK(!db.empty());
+        CHECK(db.size() == 2);
+
+        db.flush();
+    }
+    std::remove(kTestDbPath);
+}
+
+void test_kv_store_erase() {
+    std::remove(kTestDbPath);
+    {
+        KvStore db(kTestDbPath);
+
+        db.put("test1", "symbol:1");
+
+        std::string value;
+        CHECK(db.get("test1", value));
+        CHECK(value == "symbol:1");
+
+        db.erase("test1");
+        CHECK(!db.get("test1", value));
+        CHECK(db.empty());
+
+        db.flush();
+    }
+    std::remove(kTestDbPath);
+}
+
+void test_kv_store_erase_reopen() {
+    std::remove(kTestDbPath);
+    {
+        KvStore db(kTestDbPath);
+
+        db.put("test1", "symbol:1");
+        db.put("test67", "symbol:67");
+        db.erase("test1");
+        db.flush();
+    }
+    {
+        KvStore db(kTestDbPath);
+
+        std::string value;
+        CHECK(!db.get("test1", value));
+
+        CHECK(db.get("test67", value));
+        CHECK(value == "symbol:67");
+
+        CHECK(db.size() == 1);
+    }
+    std::remove(kTestDbPath);
+}
+
+void test_kv_store_compact() {
+    std::remove(kTestDbPath);
+    {
+        KvStore db(kTestDbPath);
+
+        db.put("test1", "symbol:1");
+        db.put("test67", "symbol:67");
+        db.erase("test67");
+
+        CHECK(db.size() == 1);
+
+        db.compact();
+
+        std::string value;
+        CHECK(db.get("test1", value));
+        CHECK(!db.get("test67", value));
+        CHECK(db.size() == 1);
+
+        db.flush();
+    }
+    {
+        KvStore db(kTestDbPath);
+
+        std::string value;
+        CHECK(db.get("test1", value));
+        CHECK(value == "symbol:100");
+        CHECK(!db.get("test67", value));
+        CHECK(db.size() == 1);
+    }
+    std::remove(kTestDbPath);
+}
